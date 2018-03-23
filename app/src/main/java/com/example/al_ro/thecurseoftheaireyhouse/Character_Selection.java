85738312484bbd52f15af25b74c58832c418a0cf
Alex Goodchild <32183727+apgoodchild@users.noreply.github.com>
2018-03-24 09:28:24 +1100

    Initial commit

---
+++ app/src/main/java/com/example/al_ro/thecurseoftheaireyhouse/Character_Selection.java
@@ -0,0 +1,13 @@
+package com.example.al_ro.thecurseoftheaireyhouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Character_Selection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character__selection);
+    }
+}
